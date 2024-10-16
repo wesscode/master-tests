b4a9df4a075e6a7e53012d87ba9b4abfb7f77183
wesscode <wesley.info07@gmail.com>
2024-10-15 21:14:27 -0300

    Feat - Implementando classes de exemplo para trabalhar com Fixtures

---
+++ 01 - Teste de Unidade/Feature.Tests/02 - Fixtures/ClienteTesteInvalido.cs
@@ -0,0 +1,29 @@
+﻿using Feature.Clientes;
+
+namespace Feature.Tests
+{
+    public class ClienteTesteInvalido
+    {
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Fixture Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+               Guid.NewGuid(),
+               "",
+               "",
+               DateTime.Now,
+               "edu2edu.com",
+               true,
+               DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult?.Errors.Count);
+        }
+    }
+}
