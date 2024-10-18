61235a497b59a1b9cf56051fa073325656ab2203
wesscode <wesley.info07@gmail.com>
2024-10-17 21:52:30 -0300

    Feat - Ajustes para retornar clientes diversos com bogus.

--- 01 - Teste de Unidade/Feature.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
+++ 01 - Teste de Unidade/Feature.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
@@ -10,23 +10,37 @@ namespace Features.Tests
     {
         public Cliente GerarClienteValido()
         {
+            return GerarClientes(1, true).FirstOrDefault();
+        }
+
+        public IEnumerable<Cliente> ObterClientesVariados()
+        {
+            var clientes = new List<Cliente>();
+            clientes.AddRange(GerarClientes(50, true).ToList());
+            clientes.AddRange(GerarClientes(50, false).ToList());
+
+            return clientes;
+        }
+
+        public IEnumerable<Cliente> GerarClientes(int quantidade, bool ativo)
+        {
             var genero = new Faker().PickRandom<Name.Gender>();
             //var email = new Faker().Internet.Email("wesley", "alves", "gmail");
             //var clientefaker = new Faker<Cliente>();
             //clientefaker.RuleFor(c => c.Nome, (f, c) => f.Name.FirstName());
 
-            var cliente = new Faker<Cliente>("pt_BR")
+            var clientes = new Faker<Cliente>("pt_BR")
                 .CustomInstantiator(f => new Cliente(
                     Guid.NewGuid(),
                     f.Name.FirstName(genero),
                     f.Name.LastName(genero),
                     f.Date.Past(80, DateTime.Now.AddYears(-18)),
                     "",
-                    true,
-                    DateTime.Now                    ))
+                    ativo,
+                    DateTime.Now))
                 .RuleFor(x => x.Email, (f, c) => f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
 
-            return cliente;
+            return clientes.Generate(quantidade);
         }
 
         public Cliente GerarClienteInvalido()
