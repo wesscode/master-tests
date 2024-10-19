d1f021be374cc4ee55aa12299ed8b925bd76adcf
wesscode <wesley.info07@gmail.com>
2024-10-19 16:52:40 -0300

    Feat - Implementando Skip  de testes.

---
+++ 01 - Teste de Unidade/Feature.Tests/08 - Skip/TesteNaoPassandoMotivoEspecifico.cs
@@ -0,0 +1,13 @@
+﻿namespace Features.Tests
+{
+    // Exemplo quando queremos PULAR um testes.
+    public class TesteNaoPassandoMotivoEspecifico
+    {
+        [Fact(DisplayName = "Novo Cliente 2.0", Skip ="Nova versão 2.0 quebrando")]
+        [Trait("Categoria", "Escapando dos Testes")]
+        public void Teste_NaoPassando_VersaoNovaNaoCompativel()
+        {
+            Assert.True(false);
+        }
+    }
+}
