a0848e28f517d0a42b8ab9abf35736d9d4763e10
wesscode <wesley.info07@gmail.com>
2024-10-20 19:14:11 -0300

    Feat - Implementando a funcionalidade de novo pedido da classe pedido.

---
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+        public Pedido()
+        {
+            _pedidoItems = new List<PedidoItem>();
+        }
+
+        public decimal ValorTotal { get; private set; }
+
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+
+        public void AdicionarItem(PedidoItem pedidoItem)
+        {
+            _pedidoItems.Add(pedidoItem);
+            ValorTotal = PedidoItems.Sum(i => i.Quantidade * i.ValorUnitario);
+        }
+    }
+}
---
+++ 02 - TDD/src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -0,0 +1,17 @@
+﻿namespace NerdStore.Vendas.Domain
+{
+    public class PedidoItem
+    {
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+        public PedidoItem(Guid produtoId, string produtoNome, int quatidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quatidade;
+            ValorUnitario = valorUnitario;
+        }
+    }
+}
