d0820bf83b82ca69360fe209a9208b7c42a49dcf
wesscode <wesley.info07@gmail.com>
2024-10-23 23:00:03 -0300

    Feat - Criando classe Voucher, implementando se o vucher é válido.

---
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class VoucherTests
+    {
+        [Fact(DisplayName = "Validar Voucher Tipo Valor Válido")]
+        [Trait("Categoria", "Vendas - Voucher")]
+        public void Voucher_ValidarVoucherTipoValor_DeveEstarValido()
+        {
+            // Arrange
+            var voucher = new Voucher("PROMO-15-REAIS", null, 15, 1,
+                TipoDescontoVoucher.Valor, DateTime.Now.AddDays(15), true, false);
+
+            // Act
+            var result = voucher.ValidarSeAplicavel();
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+    }
+}
