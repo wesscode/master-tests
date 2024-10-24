c4341a392581f34aaccfdcc3b2a052d6adabc632
wesscode <wesley.info07@gmail.com>
2024-10-23 23:03:58 -0300

    Feat - Validando voucher tipo porcentagem.

--- 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
@@ -40,5 +40,40 @@ namespace NerdStore.Vendas.Domain.Tests
             Assert.Contains(VoucherAplicavelValidation.UtilizadoErroMsg, result.Errors.Select(c => c.ErrorMessage));
             Assert.Contains(VoucherAplicavelValidation.ValorDescontoErroMsg, result.Errors.Select(c => c.ErrorMessage));
         }
+
+        [Fact(DisplayName = "Validar Voucher Porcentagem Válido")]
+        [Trait("Categoria", "Vendas - Voucher")]
+        public void Voucher_ValidarVoucherPorcentagem_DeveEstarValido()
+        {
+            var voucher = new Voucher("PROMO-15-OFF", 15, null, 1,
+                TipoDescontoVoucher.Porcentagem, DateTime.Now.AddDays(15), true, false);
+
+            // Act
+            var result = voucher.ValidarSeAplicavel();
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact(DisplayName = "Validar Voucher Porcentagem Inválido")]
+        [Trait("Categoria", "Vendas - Voucher")]
+        public void Voucher_ValidarVoucherPorcentagem_DeveEstarInvalido()
+        {
+            var voucher = new Voucher("", null, null, 0,
+                TipoDescontoVoucher.Porcentagem, DateTime.Now.AddDays(-1), false, true);
+
+            // Act
+            var result = voucher.ValidarSeAplicavel();
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal(6, result.Errors.Count);
+            Assert.Contains(VoucherAplicavelValidation.AtivoErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherAplicavelValidation.CodigoErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherAplicavelValidation.DataValidadeErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherAplicavelValidation.QuantidadeErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherAplicavelValidation.UtilizadoErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherAplicavelValidation.PercentualDescontoErroMsg, result.Errors.Select(c => c.ErrorMessage));
+        }
     }
 }
