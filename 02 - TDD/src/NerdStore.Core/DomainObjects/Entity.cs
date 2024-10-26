20f4d86785eb251a34ea1db20d771b8f9f479580
wesscode <wesley.info07@gmail.com>
2024-10-26 13:14:46 -0300

    Feat - Criando classes abstratas.

---
+++ 02 - TDD/src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
