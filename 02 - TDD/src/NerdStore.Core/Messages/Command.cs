3a7c7f24030b106a2c7d92590bb887ee3d0e81dc
wesscode <wesley.info07@gmail.com>
2024-10-26 11:06:59 -0300

    Feat - Adicionando camada de aplication, criando comando e testando se um comando é valido.

---
+++ 02 - TDD/src/NerdStore.Core/Messages/Command.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation.Results;
+
+namespace NerdStore.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+        public abstract bool EhValido();
+    }
+}
---
+++ 02 - TDD/tests/NerdStore.Vendas.Application.Tests/Pedidos/AdicionarItemPedidoCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using NerdStore.Vendas.Application.Commands;
+
+namespace NerdStore.Vendas.Application.Tests.Pedidos
+{
+    public class AdicionarItemPedidoCommandTests
+    {
+        [Fact(DisplayName = "Adicionar Item Command Válido")]
+        [Trait("Categoria", "Vendas - Pedido Commands")]
+        public void AdicionarItemPedidoCommand_CommandoEstaValido_DevePassarNaValidacao() //Teste_Cenario_OQueEspero
+        {
+            // Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.NewGuid(), Guid.NewGuid(), "Produto Teste", 2, 100);
+
+            // Act
+            var result = pedidoCommand.EhValido();
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
