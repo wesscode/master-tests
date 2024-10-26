20e5110e206b72955a687e1d998214280007b877
wesscode <wesley.info07@gmail.com>
2024-10-26 19:36:28 -0300

    Feat - Implementando comportamentos para events na entidade.

--- 02 - TDD/src/NerdStore.Core/DomainObjects/Entity.cs
+++ 02 - TDD/src/NerdStore.Core/DomainObjects/Entity.cs
@@ -1,12 +1,33 @@
-﻿namespace NerdStore.Core.DomainObjects
+﻿using NerdStore.Core.Messages;
+
+namespace NerdStore.Core.DomainObjects
 {
     public abstract class Entity
     {
         public Guid Id { get; set; }
 
+        private List<Event> _notificacoes;
+        public IReadOnlyCollection<Event> Notificacoes => _notificacoes?.AsReadOnly();
+
         protected Entity()
         {
             Id = Guid.NewGuid();
+        }
+
+        public void AdicionarEvento(Event evento)
+        {
+            _notificacoes = _notificacoes ?? new List<Event>();
+            _notificacoes.Add(evento);
+        }
+
+        public void RemoverEvento(Event eventoItem)
+        {
+            _notificacoes?.Remove(eventoItem);
+        }
+
+        public void LimparEventos()
+        {
+            _notificacoes?.Clear();
         }
     }
 }
