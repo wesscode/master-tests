0f9db24aa49ae029c0a3d1fa7156cbd8e935ffc9
wesscode <wesley.info07@gmail.com>
2024-11-05 21:49:39 -0300

    Feat - Criando classe de UsuarioTests

---
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -0,0 +1,15 @@
+﻿using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+
+namespace NerdStore.WebApp.Tests
+{
+    public class UsuarioTests
+    {
+        private readonly IntegrationTestsFixture<Program> _testsFixture;
+
+        public UsuarioTests(IntegrationTestsFixture<Program> testsFixture)
+        {
+            _testsFixture = testsFixture; 
+        }
+    }
+}
