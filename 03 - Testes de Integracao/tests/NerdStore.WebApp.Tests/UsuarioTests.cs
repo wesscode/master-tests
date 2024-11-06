a603b2c87d39229e7ee0054f5cbf8070818fa7a1
wesscode <wesley.info07@gmail.com>
2024-11-05 22:01:52 -0300

    Feat - Criando priemeiro teste, onde verifica se o app está respondendo na url passada.

--- 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -3,13 +3,27 @@ using NerdStore.WebApp.Tests.Config;
 
 namespace NerdStore.WebApp.Tests
 {
+    [Collection(nameof(IntegrationWebTestsFixtureCollection))]
     public class UsuarioTests
     {
         private readonly IntegrationTestsFixture<Program> _testsFixture;
 
         public UsuarioTests(IntegrationTestsFixture<Program> testsFixture)
         {
-            _testsFixture = testsFixture; 
+            _testsFixture = testsFixture;
+        }
+
+        [Fact(DisplayName = "Realizar cadastro com sucesso")]
+        [Trait("Categoria", "Integração Web - Usuário")]
+        public async Task Usuario_RealizarCadastro_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var initialResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Register");
+            initialResponse.EnsureSuccessStatusCode();
+
+            // Act
+
+            // Assert
         }
     }
 }
