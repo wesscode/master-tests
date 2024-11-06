c2bacf3bd1c07e4c6e9736cb15a03123af1402a1
wesscode <wesley.info07@gmail.com>
2024-11-05 21:45:16 -0300

    Feat - Configurações e fixtures para  preparar ambiente para tests de integração.

---
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/LojaAppFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace NerdStore.WebApp.Tests.Config
+{
+    public class LojaAppFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class //factory para subir uma aplicação web.
+    {
+        #region Código Legado
+        // Deixei comentado para manter a base histórica, o código comentado é compativel até .NET 5
+
+        //protected override void ConfigureWebHost(IWebHostBuilder builder)
+        //{
+        //    builder.UseStartup<TProgram>();
+        //    builder.UseEnvironment("Testing");
+        //}
+        #endregion
+
+        // .NET 6 em diante:
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing");
+            return base.CreateHost(builder);
+        }
+    }
+}
---
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/IntegrationTestsFixture.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+
+namespace NerdStore.WebApp.Tests
+{
+    [CollectionDefinition(nameof(IntegrationWebTestsFixtureCollection))]
+    public class IntegrationWebTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<Program>> { }
+
+    [CollectionDefinition(nameof(IntegrationApiTestsFixtureCollection))]
+    public class IntegrationApiTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<Program>> { }
+
+    public class IntegrationTestsFixture<TProgram> : IDisposable where TProgram : class
+    {
+        public readonly LojaAppFactory<TProgram> Factory;
+
+        public HttpClient Client;
+
+        public IntegrationTestsFixture()
+        {
+            var clientOptions = new WebApplicationFactoryClientOptions
+            {
+
+            };
+
+            Factory = new LojaAppFactory<TProgram>();
+            Client = Factory.CreateClient(clientOptions);
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            Factory.Dispose();
+        }
+    }
+}
