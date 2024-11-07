d97dc235c7c772f2d98192cc66a210dcae1a7bb3
wesscode <wesley.info07@gmail.com>
2024-11-06 21:04:47 -0300

    Feat - Teste de integração para registrar usuário.

--- 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -21,9 +21,28 @@ namespace NerdStore.WebApp.Tests
             var initialResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Register");
             initialResponse.EnsureSuccessStatusCode();
 
+            var email = "teste@teste.com";
+
+            var formData = new Dictionary<string, string>
+            {
+                {"Input.Email", email },
+                {"Input.Password", "Teste@123" },
+                {"Input.ConfirmPassword", "Teste@123" },
+            };
+
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Register")
+            {
+                Content = new FormUrlEncodedContent(formData)
+            };
+
             // Act
+            var postResponse = await _testsFixture.Client.SendAsync(postRequest);
 
             // Assert
+            var responseString = await postResponse.Content.ReadAsStringAsync();
+
+            postResponse.EnsureSuccessStatusCode();
+            Assert.Contains($"Hello {email}!", responseString);
         }
     }
 }
