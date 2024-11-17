5786ed98d6034b2fcf6eedf57aa708d3ca558a44
wesscode <wesley.info07@gmail.com>
2024-11-17 16:03:08 -0300

    Feat -Criando anotações de AAA para cada passo.

--- 04 - BDD - Testes de Automação/tests/NerdStore.BDD.Tests/Pedido/Pedido_AdicionarItemAoCarrinhoSteps.cs
+++ 04 - BDD - Testes de Automação/tests/NerdStore.BDD.Tests/Pedido/Pedido_AdicionarItemAoCarrinhoSteps.cs
@@ -1,4 +1,3 @@
-using System;
 using TechTalk.SpecFlow;
 
 namespace NerdStore.BDD.Tests.Pedido
@@ -7,82 +6,135 @@ namespace NerdStore.BDD.Tests.Pedido
     public class Pedido_AdicionarItemAoCarrinhoSteps
     {
         [Given(@"O usuario esteja logado")]
-        public void DadoOUsuarioEstejaLogado()
-        {
-            throw new PendingStepException();
+        public void DadoOUsuarioEstejaLogado() // Arrage do BDD
+        {            
+            //AAA do passo*
+
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Given(@"Que um produto esteja na vitrine")]
         public void DadoQueUmProdutoEstejaNaVitrine()
         {
-            throw new PendingStepException();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Given(@"Esteja disponivel no estoque")]
         public void DadoEstejaDisponivelNoEstoque()
         {
-            throw new PendingStepException();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Given(@"Não tenha nenhum produto adicionado ao carrinho")]
         public void DadonNaoTenhaNenhumProdutoAdicionadoAoCarrinho()
         {
-            throw new PendingStepException();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [When(@"O usuário adicionar uma unidade ao carrinho")]
         public void QuandoOUsuarioAdicionarUmaUnidadeAoCarrinho()
         {
-            throw new PendingStepException();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Then(@"O usuário será redirecionado ao resumo da compra")]
         public void EntãoOUsuarioSeraRedirecionadoAoResumoDaCompra()
         {
-            throw new PendingStepException();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Then(@"O valor total do pedido será exatamente o valor do item adicionado")]
         public void EntãoOValorTotalDoPedidoSeraExatamenteOValorDoItemAdicionado()
         {
-            throw new PendingStepException();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [When(@"O usuário adicionar um item acima da quantidade máxima permitida")]
         public void QuandoOUsuarioAdicionarUmItemAcimaDaQuantidadeMaximaPermitida()
         {
-            throw new PendingStepException();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Then(@"Receberá uma mensagem de erro mencionando que foi ultrapassada a quantidade limite")]
         public void EntãoReceberaUmaMensagemDeErroMencionandoQueFoiUltrapassadaAQuantidadeLimite()
         {
-            throw new PendingStepException();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Given(@"O mesmo produto já tenha sido adicionado ao carrinho anteriormente")]
         public void DadoOMesmoProdutoJaTenhaSidoAdicionadoAoCarrinhoAnteriormente()
         {
-            throw new PendingStepException();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Then(@"A quantidade de itens daquele produto terá sido acrescida em uma unidade a mais")]
         public void EntaoAQuantidadeDeItensDaqueleProdutoTeraSidoAcrescidaEmUmaUnidadeAMais()
         {
-            throw new PendingStepException();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Then(@"O valor total do pedido será a multiplicação da quantidade de itens pelo valor unitario")]
         public void EntaoOValorTotalDoPedidoSeraAMultiplicacaoDaQuantidadeDeItensPeloValorUnitario()
         {
-            throw new PendingStepException();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [When(@"O usuário adicionar a quantidade máxima permitida ao carrinho")]
         public void QuandoOUsuarioAdicionarAQuantidadeMaximaPermitidaAoCarrinho()
         {
-            throw new PendingStepException();
-        }
+            // Arrange
 
+            // Act
+
+            // Assert
+        }
     }
 }
