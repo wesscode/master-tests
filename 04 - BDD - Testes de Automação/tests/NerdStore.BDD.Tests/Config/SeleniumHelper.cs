38bae4ed03dfbbe2fe35aea7c1588d938231274d
wesscode <wesley.info07@gmail.com>
2024-11-20 07:43:28 -0300

    Feat - Implementando factory para instancia do webdriver.

--- 04 - BDD - Testes de Automação/tests/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
+++ 04 - BDD - Testes de Automação/tests/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
@@ -13,11 +13,36 @@
     public enum Browser
     {
         Chrome,
-        FireFox
+        Firefox
     }
 
     public static class WebDriverFactory
     {
+        public static IWebDriver CreateWebDriver(Browser browser, string caminhoDriver, bool headless)
+        {
+            IWebDriver webDriver = null;
 
+            switch (browser)
+            {
+                case Browser.Firefox:
+                    var optionsFireFox = new FirefoxOptions();
+                    if (headless)
+                        optionsFireFox.AddArgument("--headless");
+
+                    webDriver = new FirefoxDriver(caminhoDriver, optionsFireFox);
+
+                    break;
+                case Browser.Chrome:
+                    var options = new ChromeOptions();
+                    if (headless)
+                        options.AddArgument("--headless");
+
+                    webDriver = new ChromeDriver(caminhoDriver, options);
+
+                    break;
+            }
+
+            return webDriver;
+        }
     }
 }
